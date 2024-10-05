477b50f9477143adb82f9a7633b2c4b77a410438
Ihor Trehub <pptregubi@gmail.com>
2024-10-05 03:06:24 +0300

    Add new tests

---
+++ UniversityApp/UniversityApp.ViewModel.Tests/ViewModels/Dialogs/CourseDialogViewModelTests.cs
@@ -0,0 +1,28 @@
+﻿using UniversityApp.ViewModel.ViewModels.Dialogs;
+
+namespace UniversityApp.ViewModel.Tests.ViewModels.Dialogs;
+
+public class CourseDialogViewModelTests
+{
+    private readonly CourseDialogViewModel _vm = new CourseDialogViewModel(string.Empty, () => { });
+
+    [Fact]
+    public void CourseDialogViewModel_OkCommand_Test()
+    {
+        var expected = true;
+        _vm.OkCommand.Execute(null);
+        var actual = _vm.IsSuccess;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void CourseDialogViewModel_CancelCommand_Test()
+    {
+        var expected = false;
+        _vm.CancelCommand.Execute(null);
+        var actual = _vm.IsSuccess;
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ UniversityApp/UniversityApp.ViewModel.Tests/ViewModels/Dialogs/GroupDialogViewModelTests.cs
@@ -0,0 +1,52 @@
+﻿using UniversityApp.Model.Interfaces;
+using UniversityApp.Model.Tests;
+using UniversityApp.ViewModel.ViewModels.Dialogs;
+
+namespace UniversityApp.ViewModel.Tests.ViewModels.Dialogs;
+
+public class GroupDialogViewModelTests
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly GroupDialogViewModel _vm;
+
+    public GroupDialogViewModelTests()
+    {
+        _unitOfWork = new UnitOfWorkFake();
+        _unitOfWork.TeacherRepository.Create(new Model.Entities.Teacher("FirstName", "SecondName"));
+        _unitOfWork.TeacherRepository.Create(new Model.Entities.Teacher("One", "Two"));
+        _unitOfWork.CourseRepository.Create(new Model.Entities.Course("Test1"));
+
+        _vm = new GroupDialogViewModel(string.Empty, () => { }, _unitOfWork);
+    }
+
+    [Fact]
+    public async Task GroupDialogViewModel_LoadAllDataCommand_Test()
+    {
+        var expected = true;
+        await _vm.LoadAllDataCommand.ExecuteAsync(null);
+        var actual = _vm.Courses.Any(c => c.Name == "Test1") &&
+            _vm.Teachers.All(t => t.FirstName == "One" || t.FirstName == "FirstName");
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void GroupDialogViewModel_OkCommand_Test()
+    {
+        var expected = true;
+        _vm.OkCommand.Execute(null);
+        var actual = _vm.IsSuccess;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void GroupDialogViewModel_CancelCommand_Test()
+    {
+        var expected = false;
+        _vm.CancelCommand.Execute(null);
+        var actual = _vm.IsSuccess;
+
+        Assert.Equal(expected, actual);
+    }
+}
